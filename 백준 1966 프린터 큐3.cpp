d1db96843bb72e57e2e7e3a7b25e2334f3a1df26
namyoungu <filekkk123@naver.com>
2019-04-08 23:38:22 +0900

    Create 백준 1966 프린터 큐3.cpp

---
+++ 백준 1966 프린터 큐3.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNING
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+queue <int> q;
+queue <int> ind;
+queue <int> imp_s;
+
+int main(void) {
+	int t_s, n[100], m[100], M, tmp, tmp2, t_i, count;
+	int imp[100][100], tmp_imp[100];
+
+	scanf("%d", &t_s);
+	for (int i = 0; i < t_s; i++) {
+		scanf("%d %d", &n[i], &m[i]);
+		for (int j = 0; j < n[i]; j++) {
+			scanf("%d", imp[i]);
+		}
+	}
+
+	for (int i = 0; i < t_s; i++) {
+		for (int z = 0; z < n[i]; z++) {
+			q.push()
+		}
+
+
+
+
+
+	}
+
+
+
+
+
+
+	return 0;
+}
