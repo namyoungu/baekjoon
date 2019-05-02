6124387517d7b0a342bdf948a89120830cb087fe
namyoungu <filekkk123@naver.com>
2019-05-03 02:57:48 +0900

    Create 백준 15652번 n과m (4).cpp

---
+++ 백준 15652번 n과m (4).cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+
+int n, m, s[9];
+
+void go(int dep) {
+	if (dep == m) {
+		for (int i = 0; i < m; i++) {
+			printf("%d ", s[i]);
+		}
+		puts("");
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (dep > 0 && s[dep - 1] >= i) {
+			continue;
+		}
+		s[dep] = i;
+
+		go(dep + 1);
+	}
+
+}
+
+int main(void) {
+	scanf("%d %d", &n, &m);
+
+	go(0);
+
+	return 0;
+}
