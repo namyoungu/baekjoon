587f77a0099226e8baae5f09e3b005a7f298914b
namyoungu <filekkk123@naver.com>
2019-05-06 12:02:19 +0900

    Create 계산기.cpp

---
+++ 계산기.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+void stk_cal(){
+	if (str1[i] == '(') {
+
+
+	}
+
+	
+}
+
+int main(void) {
+	vector<int> v;
+	string str1;
+	char s[30];
+
+	cin >> str1;
+
+	for (int i = 0; i < str1.size(); i++) {
+		if (str1[i] == '+' || str1[i] == '-' || str1[i] == '*' || str1[i] == '/' ||
+			str1[i] == '(' || str1[i] == ')' || str1[i] == '^') {
+
+		}
+	}
+
+	return 0;
+}
